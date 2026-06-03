adc99525e4c10b694b1c5ddb391d5ea937efb922
calmonion <calmonion@calmonionui-MacBookPro.local>
2026-06-03 09:13:58 +0900

    feat: Market 페이지에 LeverageSection 마운트

--- frontend/src/pages/Market.jsx
+++ frontend/src/pages/Market.jsx
@@ -6,6 +6,7 @@ import EconIndicatorsSection from '../components/market/EconIndicatorsSection'
 import M7EarningsSection from '../components/market/M7EarningsSection'
 import KrTop2Section from '../components/market/KrTop2Section'
 import KrExportsSection from '../components/market/KrExportsSection'
+import LeverageSection from '../components/market/LeverageSection'
 
 export default function Market() {
   return (
@@ -19,6 +20,7 @@ export default function Market() {
       <M7EarningsSection />
       <KrTop2Section />
       <KrExportsSection />
+      <LeverageSection />
     </div>
   )
 }
